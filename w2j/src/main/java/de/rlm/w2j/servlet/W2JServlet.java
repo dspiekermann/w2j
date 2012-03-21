eb791ec481a5e2bf493abcf777414b3d01919968
Dennis <Dennis@172.17.1.82>
2012-03-21 13:25:03 +0100

    Servlets added

---
+++ w2j/src/main/java/de/rlm/w2j/servlet/W2JServlet.java
@@ -0,0 +1,47 @@
+package de.rlm.w2j.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import de.rlm.w2j.test.Test;
+
+/**
+ * Servlet implementation class W2JServlet
+ */
+@WebServlet("/W2JServlet")
+public class W2JServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private static final Log LOG = LogFactory.getLog(Test.class); 
+	
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		LOG.info("servlet init");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		LOG.info("servlet get");
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		LOG.info("servlet post");
+	}
+
+}
